0c2c3174290e400c40848cfc259e328f544b6305
DragosP2503 <144803698+DragosP2503@users.noreply.github.com>
2023-10-02 12:13:31 +0300

    Create db.php

---
+++ db.php
@@ -0,0 +1,11 @@
+// servername => localhost
+        // username => root
+        // password => empty
+        // database name => eurocup
+        $conn = mysqli_connect("localhost", "root", "", "eurocup");
+         
+        // Check connection
+        if($conn === false){
+            die("ERROR: Could not connect. "
+                . mysqli_connect_error());
+        }
